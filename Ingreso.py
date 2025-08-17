d7f1ff1e4fd9c960de14e9e2e15b8564695a27f2
Marvin <sonmarvint2001@gmail.com>
2025-08-16 22:36:40 -0600

    Avance ingreso

--- Ingreso.py
+++ Ingreso.py
@@ -16,8 +16,31 @@ def ingresar():
         except ValueError:
             print("El codigo del producto no existe")
     while True:
-        nombreAux=input("Ingrese el nombre del producto: ")
-        if nombreAux != "" :
-            break
-        else:
+        try:
+            nombreAux=input("Ingrese el nombre del producto: ")
+            if nombreAux != "" :
+                break
+            else:
+                print("El nombre no puede estar en blanco")
+        except ValueError:
             print("El nombre no puede estar en blanco")
+    while True:
+        try:
+            print("Categorias disponibles: ")
+            print("1. Encendedores")
+            print("2. Puros")
+            print("3. Miselianas")
+            opcion=input("Ingrese el numero de categoria")
+            if opcion == "1":
+                categoriaAux="Encendedores"
+                break
+            elif opcion == "2":
+                categoriaAux="Puros"
+                break
+            elif opcion == "3":
+                categoriaAux="Miselianas"
+                break
+            else:
+                print("Categoria no existente")
+        except ValueError:
+            print("Categoria no existente")
